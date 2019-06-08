e23ad3b2cc7590680d5f49c8015ef9ab620a8888
lloan <lloanalas@outlook.com>
2019-06-07 20:57:29 -0700

    custom express server

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({dev});
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.get('/post/:id', (req, res) => {
+    const actualPage = '/post';
+    const queryParams = {slug: req.params.id};
+    app.render(req, res, actualPage, queryParams);
+  });
+
+  server.get('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(5100, err => {
+    if (err) throw err;
+    console.log('> Ready on http://localhost:5100');
+  });
+}).catch(ex => {
+  console.error(ex.stack);
+  process.exit(1);
+});
