23440a71464f3afc4db2f0e46e9f9c57dd23f13c
lloan <lloanalas@outlook.com>
2019-06-13 14:02:56 -0700

    blocking out footer

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-unused-vars */
 // eslint-disable-next-line no-unused-vars
 import React, {useState, useEffect} from 'react';
+import Footer from '../src/components/footer';
 import '../node_modules/@fortawesome/fontawesome-free/js/all';
 import '../sass/index.scss';
 
@@ -8,16 +10,18 @@ import {withRouter} from 'next/router';
 // import Fetch from 'isomorphic-unfetch';
 function App() {
   // basic useState hook
-  const [test, setTest] = useState('Hello'); // getter, setter = defaultValue
+  // const [test, setTest] = useState('Hello'); // getter, setter = defaultValue
 
   // effect (apis, asynchronous stuff, etc.)
-  useEffect(() => {
-    setTimeout(()=> setTest('Goodbye'), 5000);
-  });
+  // useEffect(() => {
+  //   setTimeout(()=> setTest('Goodbye'), 5000);
+  // });
 
   // initial render
   return (
-    <h1>{test} IESD</h1>
+    <div>
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/components/footer.js
@@ -0,0 +1,24 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+function Footer() {
+  // initial render
+  return (
+    <footer className="full-width">
+      <div className="grid-container">
+        <div className="column-24 leader-2">
+          <div className="column-6">
+            <div><img src="../../static/logos/iesd-logo-white.svg"/></div>
+            <div>slogan</div>
+            <div>footer text</div>
+          </div>
+          <div className="column-18">
+            <div>big ass text</div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
