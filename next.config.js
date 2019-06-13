86fb0651ed3ead35f7c8bbd8b571083eed3fe27a
lloan <lloanalas@outlook.com>
2019-06-13 13:59:58 -0700

    updated base nextJS setup

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const withCSS = require('@zeit/next-css'),
-    withSASS = require('@zeit/next-sass'),
-    withImages = require('next-images');
+  withSASS = require('@zeit/next-sass'),
+  withImages = require('next-images');
 
 module.exports = withImages(withCSS(withSASS()));
