8e4d0401dbac7f2195c31dedf0da610903e5e5b8
Amber Stumpf <aiyemacbain@gmail.com>
2019-06-16 15:51:54 -0700

    Initial commit

---
+++ src/components/leadership.js
@@ -0,0 +1,9 @@
+class Leadership extends React.Component {
+    render() {
+        <div className="grid-container">
+            <h1>Hello</h1>
+        </div>
+    }
+}
+
+export default Leadership;
