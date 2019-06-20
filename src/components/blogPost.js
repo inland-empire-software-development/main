c2a2fc00e9602ba08e1a1506808219210feb9d9e
rurru <rpreibisius@gmail.com>
2019-06-19 21:16:57 -0700

    blogPost initial commit

---
+++ src/components/blogPost.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class BlogPost extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: [],
+    };
+  }
+
+  render() {
+    return {};
+  }
+}
+
+export default BlogPost;
