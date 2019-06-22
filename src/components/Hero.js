d955b2557ede2a3b5c5b3c80ef20e9a6342520d3
David <daveshuang@gmail.com>
2019-06-22 10:22:45 -0700

    created hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,20 +3,20 @@
  * @return {Hero}
  */
 function Hero() {
-    return (
-        <header id="hero" className="grid-container">
-            <div className="column-24">
-                {/* Navigation */}
-                <nav></nav>
+  return (
+    <header id="hero" className="grid-container">
+      <div className="column-24">
+        {/* Navigation */}
+        <nav></nav>
 
-                {/* Side bar */}
-                <div></div>
+        {/* Side bar */}
+        <div></div>
 
-                {/* Event */}
-                <div></div>
-            </div>
-        </header>
-    )
+        {/* Event */}
+        <div></div>
+      </div>
+    </header>
+  );
 }
 
 export default Hero;
