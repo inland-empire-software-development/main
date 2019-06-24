6d3bdd2397f7c39acd4e424ac751240dc53f0540
tonynguyen111997 <tonynguyen111997@gmail.com>
2019-06-24 02:49:00 -0700

    update successStoriesData.js

--- src/utils/successStoriesData.js
+++ src/utils/successStoriesData.js
@@ -7,11 +7,7 @@ export const data = [
 		excerpt: "Lorem ipsum dolor sit amet, consectetur	adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 		linkToStory: '#',
 		socialNetworks: {
-			github: 'github.com',
-			linkedin: 'linkedin.com',
-			stackoverflow: 'stackoverflow.com',
 			facebook: 'facebook.com',
-			instagram: 'instagram.com',
 			twitter: 'twitter.com',
 		}
 	},
@@ -20,7 +16,10 @@ export const data = [
 		name: 'John Doe',
 		title: 'Pizza boy',
 		excerpt: "John Doe Lorem ipsum dolor sit amet, consectetur	adipiscing eaolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-		linkToStory: '#'
+		linkToStory: '#',
+		socialNetworks: {
+			facebook: 'facebook.com',
+		}
 	},
 	{
 		imageURL: 'https://user-images.githubusercontent.com/36907562/59642799-56ec9300-911b-11e9-899b-b9b555a29b8a.png',
