e9950ed6dab101a29eab6cce8ea82b6f2f3bde2a
Amber Stumpf <aiyemacbain@gmail.com>
2019-07-03 12:50:45 -0700

    Adds gradient color over image

--- src/components/leadership/leadership-image.js
+++ src/components/leadership/leadership-image.js
@@ -1,9 +1,11 @@
 const LeadershipImage = (props) => (
   <div id='leadership-image' >
     <div className='card profile-photo'>
-      <img className='card-image' src={props.image} alt='Profile picture' />
-    </div>
+      <img className='card-image' src={props.image} alt='Profile picture'
+      />
+      <div className='overlay'></div>
 
+    </div>
     <div className='card name-title'>
       <div className='card-content'>
         <p className='leadership-name'>{props.userName}<br />
