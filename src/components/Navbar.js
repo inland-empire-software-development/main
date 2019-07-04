13a44e33881bb1b6ab712ab4abe13adfcd801703
tonynguyen111997 <tonynguyen111997@gmail.com>
2019-07-04 01:15:50 -0700

    Add isMobile util function

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,15 @@
+import { useState, useEffect } from 'react';
+
 //components
 import NavbarDesktop from './NavbarDesktop';
 import NavbarMobile from './NavbarMobile';
 
+//utils
+import {isMobile} from '../utils/isMobile';
+
 function Navbar(){
-	const navbar = true ? <NavbarDesktop /> : <NavbarMobile />
+	const navbar = isMobile() ? <NavbarMobile /> : <NavbarDesktop />;
+	
 	return (
 		<div>
 			{navbar}
