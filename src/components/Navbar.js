591bc26ec027eed0b8dbe7319f1b34fa0560ba32
tonynguyen111997 <tonynguyen111997@gmail.com>
2019-07-04 19:12:00 -0700

    Resolve yarn lint issues

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,31 @@
-import { useState, useEffect } from 'react';
+import {useState, useEffect} from 'react';
 
-//components
+// components
 import NavbarDesktop from './NavbarDesktop';
 import NavbarMobile from './NavbarMobile';
 
-//utils
+// utils
 import {isMobile} from '../utils/isMobile';
 
-function Navbar(){
-	const [navbar, setNavBar] = useState(null);
+function Navbar() {
+  const [navbar, setNavBar] = useState(null);
 
-	// Uses the util function isMobile to 
-	// check if screen width is below 860px
-	// Displays appropriate navbar for screen width
-	useEffect(() => {
-		if(isMobile()){
-			setNavBar(<NavbarMobile />);
-		} else {
-			setNavBar(<NavbarDesktop />);
-		}
-	});
+  // Uses the util function isMobile to
+  // check if screen width is below 860px
+  // Displays appropriate navbar for screen width
+  useEffect(() => {
+    if (isMobile()) {
+      setNavBar(<NavbarMobile />);
+    } else {
+      setNavBar(<NavbarDesktop />);
+    }
+  });
 
-	return (
-		<div id="navbar">
-			{navbar}
-		</div>
-	);
+  return (
+    <div id="navbar">
+      {navbar}
+    </div>
+  );
 }
 
 export default Navbar;
--- src/utils/isMobile.js
+++ src/utils/isMobile.js
@@ -1,5 +1,5 @@
 // Device is mobile if screen width
 // is less than or equal to 860px
 export const isMobile = () => {
-	return window.innerWidth <= 860;
-}
+  return window.innerWidth <= 860;
+};
