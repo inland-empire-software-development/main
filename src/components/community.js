d21182bc104eb17dd277ee0a3ac8256227c30be4
VelazquezLuis <javiervelazquez113@yahoo.com>
2019-07-04 19:09:45 -0700

    added community pics

--- src/components/community.js
+++ src/components/community.js
@@ -1,41 +1,60 @@
 import Swiper from 'swiper';
 import '../../node_modules/swiper/dist/css/swiper.min.css';
-
+// import React, { useState, useEffect } from 'react';
 
 let swiper = new Swiper('.swiper-container', {
-  slidesPerView: 4,
+  slidesPerView: 'auto',
+  spaceBetween: 10,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 });
 
+// function myFunction() {
+//   document.getElementById("myCirlce").style.display = "none";
+// }
+
+// function myFunction() {
+//   const x = document.getElementById("myCirlce");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   }
+// }
+
+// const cir = document.document.getElementById('myCirlce');
+
+// cir.addEventListener('click', event => {
+//   cir.style.display = "none";
+// });
+
+
 function Community() {
+  // const [x, y] = useState(null);
+
+  // // Similar to componentDidMount and componentDidUpdate:
+  // useEffect(() => {
+  //     const x = document.getElementById("myCirlce");
+  //   }
+  // });
   return (
     <div id="iesdBg" className="grid-container  ">
-      <div className="column-24 ">
-        <div className="swiper-container leader-2 trailer-2">
-          <div className="swiper-wrapper">
-            <div id="spred" className="swiper-slide">C/O https://placeholder.com/</div>
-            <div id="spred" className="swiper-slide">C/O https://placeholder.com/</div>
-            <div id="spred" className="swiper-slide">C/O https://placeholder.com/</div>
-            <div id="spred" className="swiper-slide">C/O https://placeholder.com/</div>
-            <div id="spred" className="swiper-slide">C/O https://placeholder.com/</div>
-            <div id="spred" className="swiper-slide">C/O https://placeholder.com/</div>
-            <div id="spred" className="swiper-slide">C/O https://placeholder.com/</div>
-            <div id="spred" className="swiper-slide">C/O https://placeholder.com/</div>
-            <div id="spred" className="swiper-slide">C/O https://placeholder.com/</div>
-            <div id="spred" className="swiper-slide">C/O https://placeholder.com/</div>
-            <div id="spred" className="swiper-slide">C/O https://placeholder.com/</div>
-            <div id="spred" className="swiper-slide">C/O https://placeholder.com/</div>
-            <div id="spred" className="swiper-slide">C/O https://placeholder.com/0</div>
+      <div className="column-24 " onclick="myFunction()" >
+        <div className="swiper-container leader-2 trailer-2" >
+          <div className="swiper-wrapper" >
+            <div className="swiper-slide"><img id="communitypic" src="../../static/images/community/community_1.jpg" alt="community img"></img></div>
+            <div className="swiper-slide"><img id="communitypic" src="../../static/images/community/community_2.jpg" alt="community img"></img></div>
+            <div className="swiper-slide"><img id="communitypic" src="../../static/images/community/community_3.jpg" alt="community img"></img></div>
+            <div className="swiper-slide"><img id="communitypic" src="../../static/images/community/community_4.jpg" alt="community img"></img></div>
+            <div className="swiper-slide"><img id="communitypic" src="../../static/images/community/community_5.jpg" alt="community img"></img></div>
+            <div className="swiper-slide"><img id="communitypic" src="../../static/images/community/community_6.jpg" alt="community img"></img></div>
           </div>
           <div className="swiper-button-prev">
             <span className="swiper-button-prev-lg">&lt;</span>
             <span className="swiper-button-prev-sm">&lt;</span>
           </div>
           <div className="circle_container">
-            <div className="circle">Drag</div>
+            <div id="myCirlce" className="circle">Drag</div>
           </div>
           <div className="swiper-button-next">
             <span className="swiper-button-next-sm">&gt;</span>
