fb2bbddb732730d8c86a8dbeb6c36a22838ba2b8
holyoke <jasonebueng@gmail.com>
2019-07-04 19:26:07 -0700

    introduces meetApi to events component hooks

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,8 +1,23 @@
+import {fetchEvents} from '../utils/meetupApiUtil';
+import { useState, useEffect } from 'react';
+
 function Events() {
+    const [events, setEvents] = useState([]);
+    const getEvents = fetchEvents
+
+
+    useEffect(() => {
+      async function fetchData() {
+        // You can await here
+        const response = await fetchEvents()
+        return response
+      }
+      console.log(fetchData());
+    }, []);
 
     return (
       <div className="events">
-        Test
+        {events}
       </div>
     )
   }
--- src/utils/meetupApiUtil.js
+++ src/utils/meetupApiUtil.js
@@ -3,14 +3,13 @@ import Fetch from 'isomorphic-unfetch';
 export const fetchEvents = () => {
     let id = "iesd-meetup"
     let meetApi = `https://api.meetup.com/${id}/`
-    
-    return Fetch(meetApi)
+    let config = { mode: 'no-cors'}
+
+    return Fetch(meetApi, config)
     .then(function(response) {
-      return response.json();
+      return response
     })
     .then(function(myJson) {
-      console.log(JSON.stringify(myJson));
+      return JSON.stringify(myJson);
     })
 }
-
-module.exports = fetchEvents();
