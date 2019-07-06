0b0a49ced36cd45b7fea33f3db49269601b27a69
johnathantng <johnathan.tng@gmail.com>
2019-07-06 03:26:34 -0700

    ran yarn lint and fixed error, fixed eventLink not going to correct rsvp page

--- src/components/heroEvent.js
+++ src/components/heroEvent.js
@@ -1,70 +1,72 @@
-import React, { useState, useEffect } from 'react';
-import { calcStartTime, calcEndTime } from '../utils/convertTimeUtil';
-import { splitMonth, splitDay } from '../utils/splitDateUtil';
+import {useState, useEffect} from 'react';
+import {calcStartTime, calcEndTime} from '../utils/convertTimeUtil';
+import {splitMonth, splitDay} from '../utils/splitDateUtil';
 
 function HeroEvent() {
-	const [eventName, setEventName] = useState('');
-	const [eventMonth, setEventMonth] = useState('');
-	const [eventDay, setEventDay] = useState('');
-	const [eventStartTime, setEventStartTime] = useState('');
-	const [eventEndTime, setEventEndTime] = useState('');
-	const [eventLink, setEventLink] = useState('');
+  const [eventName, setEventName] = useState('');
+  const [eventMonth, setEventMonth] = useState('');
+  const [eventDay, setEventDay] = useState('');
+  const [eventStartTime, setEventStartTime] = useState('');
+  const [eventEndTime, setEventEndTime] = useState('');
+  const [eventLink, setEventLink] = useState('');
 
-	useEffect(() => {
-		fetch("https://cors-anywhere.herokuapp.com/https://api.meetup.com/iesd-meetup/events?&sign=true&photo-host=public&page=20")
-    .then(response => response.json())
-    .then(result => { 
-    	setEventName(result[0].name);
-    	setEventStartTime(calcStartTime(result[0].local_time));
-    	setEventEndTime(calcEndTime(result[0].local_time, result[0].duration));
-    	setEventMonth(splitMonth(result[0].local_date));
-    	setEventDay(splitDay(result[0].local_date));
-    	setEventLink(result[0].link);
-		})
-	})
+  useEffect(() => {
+    fetch("https://cors-anywhere.herokuapp.com/https://api.meetup.com/iesd-meetup/events?&sign=true&photo-host=public&page=20")
+        .then((response) => response.json())
+        .then((result) => {
+          setEventName(result[0].name);
+          setEventStartTime(calcStartTime(result[0].local_time));
+          setEventEndTime(
+              calcEndTime(result[0].local_time, result[0].duration
+              ));
+          setEventMonth(splitMonth(result[0].local_date));
+          setEventDay(splitDay(result[0].local_date));
+          setEventLink(result[0].link);
+        });
+  });
 
-	return (
-		<div className="hero-event-container">
+  return (
+    <div className="hero-event-container">
 
-			{/* event details container */}
-			<div className="hero-event-info">
+      {/* event details container */}
+      <div className="hero-event-info">
 
-				{/* hero event info left side */}
-				<div className="hero-event-date">
-					
-					<p className="hero-event-day">{eventDay}</p>
-					<p className="hero-event-month">{eventMonth}</p>
-					<p className="hero-event-time">{eventStartTime} - {eventEndTime}</p>
+        {/* hero event info left side */}
+        <div className="hero-event-date">
 
-				</div>
+          <p className="hero-event-day">{eventDay}</p>
+          <p className="hero-event-month">{eventMonth}</p>
+          <p className="hero-event-time">{eventStartTime} - {eventEndTime}</p>
 
-				{/* hero event info right side */}
-				<div className="hero-event-desc">
+        </div>
 
-					<p className="event-name">{eventName}</p>
+        {/* hero event info right side */}
+        <div className="hero-event-desc">
 
-					<div className="hero-event-location">
+          <p className="event-name">{eventName}</p>
 
-						<p>Address:</p>
-						<p>3499 Tenth St. Riverside, CA 92501</p>
+          <div className="hero-event-location">
 
-					</div>
+            <p>Address:</p>
+            <p>3499 Tenth St. Riverside, CA 92501</p>
 
-				</div>
+          </div>
 
-			</div>
+        </div>
 
-			{/* reserve a spot buton */}
-			<div className="reserve-wrapper">
+      </div>
 
-				<button 
-					onClick={() => window.open('https://www.meetup.com/iesd-meetup/events/xrdssqyzjbmc/')}
-					className="reserve-button">reserve a spot</button>
+      {/* reserve a spot buton */}
+      <div className="reserve-wrapper">
 
-			</div>
+        <button
+          onClick={() => window.open(`${eventLink}`)}
+          className="reserve-button">reserve a spot</button>
 
-		</div>
-	);
+      </div>
+
+    </div>
+  );
 }
 
 export default HeroEvent;
--- src/utils/splitDateUtil.js
+++ src/utils/splitDateUtil.js
@@ -1,26 +1,26 @@
 const months = {
-    '01' : 'Jan',
-    '02' : 'Feb',
-    '03' : 'Mar',
-    '04' : 'Apr',
-    '05' : 'May',
-    '06' : 'Jun',
-    '07' : 'Jul',
-    '08' : 'Aug',
-    '09' : 'Sep',
-    '10' : 'Oct',
-    '11' : 'Nov',
-    '12' : 'Dec'
-}
+  '01': 'Jan',
+  '02': 'Feb',
+  '03': 'Mar',
+  '04': 'Apr',
+  '05': 'May',
+  '06': 'Jun',
+  '07': 'Jul',
+  '08': 'Aug',
+  '09': 'Sep',
+  '10': 'Oct',
+  '11': 'Nov',
+  '12': 'Dec',
+};
 
 export const splitMonth = (date) => {
-    const dateSplit = date.split("-");
+  const dateSplit = date.split("-");
 
-    return months[dateSplit[1]];
-}
+  return months[dateSplit[1]];
+};
 
 export const splitDay = (date) => {
-    const dateSplit = date.split("-");
+  const dateSplit = date.split("-");
 
-    return dateSplit[2];
-}
+  return dateSplit[2];
+};
