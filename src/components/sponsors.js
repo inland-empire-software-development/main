53b36d0d66738aa6c6f72ca4e7b7ce829c17b09c
Elyse Lam <30991021+elyselam@users.noreply.github.com>
2019-07-06 19:53:12 -0700

    set up html with images

--- src/components/sponsors.js
+++ src/components/sponsors.js
@@ -1,7 +1,16 @@
 function Sponsors() {
 // initial render
 return (
-	<div>sponsorship</div>
+	<div className='sponsors-container'>
+		<h1>OUR SPONSORS</h1>
+		<p>The work of Inland Empire Software Development, IESD, is made possible by the following sponsors</p>
+		<div className='logos-container'>
+			<img src='../../static/images/desktop/UCRlogo.png'/>
+			<img src='../../static/images/desktop/ExCITElogo.jpg'/>
+
+		</div>
+	</div>
+		
 	)
 
 }
