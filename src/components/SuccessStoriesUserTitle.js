b0060d902e8b987c1465998f3f52d4b5e199cac9
tonynguyen111997 <tonynguyen111997@gmail.com>
2019-07-10 22:01:15 -0700

    Changed red slash to be a span tag

--- src/components/SuccessStoriesUserTitle.js
+++ src/components/SuccessStoriesUserTitle.js
@@ -5,8 +5,8 @@ function SuccessStoriesUserTitle(props) {
     <div className="success-stories-user-info">
       <div className="success-title-container">
         <div className="success-name-container">
-          <p className="success-name">{name}</p>
-          <p className="success-slash">/</p>
+          <p className="success-name">{name}<span>/</span></p>
+          {/*<p className="success-slash">/</p>*/}
         </div>
         <p className="success-job-title">{title}</p>
       </div>
