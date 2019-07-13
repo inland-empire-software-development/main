e6cf96b515ef0beee4135ed4cfa502ea4440850d
VelazquezLuis <javiervelazquez113@yahoo.com>
2019-07-13 16:37:14 -0700

    adde on hover display none to cir

--- src/components/community.js
+++ src/components/community.js
@@ -1,20 +1,7 @@
 import Swiper from 'swiper';
 import '../../node_modules/swiper/dist/css/swiper.min.css';
 import React, { useEffect } from 'react';
-// import window from 'global.window';
 
-
-// function myFunction(numS) {
-//   if (numS.matches) { // If media query matches
-//     x = 2;
-//   } else {
-//     x= 4;
-//   }
-// }
-
-// let numS = window.matchMedia("(max-width: 700px)")
-// myFunction(numS); // Call listener function at run time
-// numS.addListener(myFunction); // Attach listener function on state changes
 
 let swiper = new Swiper('.swiper-container', {
   slidesPerView: 'auto',
@@ -25,52 +12,31 @@ let swiper = new Swiper('.swiper-container', {
   },
 });
 
-// function myFunction() {
-//   document.getElementById("myCirlce").style.display = "none";
-// }
-// const cir = document.document.getElementById('myCirlce');
-
 
 const commPic = [
-  {id: 1, image: '../../static/images/community/community_1.jpg',
+  {key: 1, image: '../../static/images/community/community_1.jpg',
     alt: 'community pic'},
-  {id: 2, image: '../../static/images/community/community_2.jpg', 
+  {key: 2, image: '../../static/images/community/community_2.jpg', 
     alt: 'community pic'},
-  {id: 3, image: '../../static/images/community/community_3.jpg', 
+  {key: 3, image: '../../static/images/community/community_3.jpg', 
     alt: 'community pic'},
-  {id: 4, image: '../../static/images/community/community_4.jpg', 
+  {key: 4, image: '../../static/images/community/community_4.jpg', 
     alt: 'community pic'},
-  {id: 5, image: '../../static/images/community/community_5.jpg', 
+  {key: 5, image: '../../static/images/community/community_5.jpg', 
     alt: 'community pic'},
-  {id: 6, image: '../../static/images/community/community_6.jpg', 
+  {key: 6, image: '../../static/images/community/community_6.jpg', 
     alt: 'community pic'},
 ];
 
 
 function Community() {
-  // const [x, y] = useState(null);
-  // function myFunction() {
-  //   const x = document.getElementById("myCirlce");
-  //   if (x.style.display === "block") {
-  //     x.style.display = "none";
-  //   }
-  //   console.log("hi");
-  // }
-
-  // Similar to componentDidMount and componentDidUpdate:
-  // useEffect(() => {
-  //   const x = document.getElementById("myCirlce");
-  //   x.addEventListener('click', event => {
-  //     myFunction()
-  //   });
-  // });
   return (
     <div className="grid-container community  ">
-      <div className="column-24 " onClick="myFunction()" >
+      <div className="column-24 " >
         <div id="iesdBg" className="swiper-container leader-1 trailer-1" >
           <div className="swiper-wrapper" >
-            { commPic.map(({id, image, alt}) => <div className="swiper-slide"> 
-              <img id="communitypic" key={id} src={image}  alt={alt} /> </div>)}
+            { commPic.map(({key, image, alt}) => <div className="swiper-slide">
+              <img id="communitypic" key={key} src={image} alt={alt} /> </div>)}
           </div>
           <div className="swiper-button-prev">
             <span className="swiper-button-prev-lg">&lt;</span>
@@ -84,12 +50,7 @@ function Community() {
             <span className="swiper-button-next-lg">&gt;</span>
           </div>
         </div>
-        {/* <div>
-          <p>You clicked {count} times</p>
-          <button onClick={() => setCount(count + 1)}>
-            Click me
-          </button>
-        </div> */}
+
       </div>
     </div>
   );
