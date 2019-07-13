33d8cefb1a454ad714b48ae2e02fce11b1b69a45
yzhan407 <hehehe2333@hotmail.com>
2019-07-13 16:02:12 -0700

    small modify

--- src/components/floatingBar.js
+++ src/components/floatingBar.js
@@ -2,10 +2,10 @@ function FloatingBar() {
   return (
     <floatingBar className="floating-bar-main">
       <div className="left-floating-bar">
-        <a href="tel:18004370267">
+        <a target="_blank" href="tel:18004370267">
           <i class="fas fa-phone" data-fa-transform="right-10 up-40"></i>
         </a>
-        <a href="mailto:community@ie-sd.com">
+        <a target="_blank" href="mailto:community@ie-sd.com">
           <i class="fas fa-envelope" data-fa-transform="right-10 up-30"></i>
         </a>
         <a>
@@ -16,18 +16,18 @@ function FloatingBar() {
       <div className="floating-empty-div">
       </div>
       <div className="right-floating-bar">
-        <a>
+        <a target="_blank" href="https://twitter.com/iesdevinc">
           <i class="fab fa-twitter" data-fa-transform="right-16 up-119"></i>
         </a>
-        <a href="https://www.facebook.com/groups/IESoftwareDevelopment/">
+        <a target="_blank" href="https://www.facebook.com/groups/IESoftwareDevelopment/">
           <i class="fab fa-facebook-f" data-fa-transform="right-16 up-103"></i>
         </a>
         <div className="share-inner-div">share:</div>
         <hr className="floating-bar-inner-line"/>
-        <a href="https://www.meetup.com/iesd-meetup/">
+        <a target="_blank" href="https://www.meetup.com/iesd-meetup/">
           <i class="fab fa-meetup" data-fa-transform="right-14 up-103"></i>
         </a>
-        <a href="https://bit.ly/2C0umfQ">
+        <a target="_blank" href="https://bit.ly/2C0umfQ">
           <i class="fab fa-slack" data-fa-transform="right-14 up-90"></i>
         </a>
         <div className="join-inner-div">join:</div>
