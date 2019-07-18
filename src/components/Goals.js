d2811d1c08b79222d0e4fd12ec0aa40a1d95ed79
johnathantng <johnathan.tng@gmail.com>
2019-07-18 01:45:04 -0700

    updated divs and updated css to remove br

--- src/components/Goals.js
+++ src/components/Goals.js
@@ -19,34 +19,26 @@ function Goals() {
         <div id="goals-right" className="column-10 post-2
         tablet-column-12 phone-post-0">
 
-          {/* goals information*/}
-          <div className="goals-info">
-
             {/* goals heading*/}
             <p className="goals-heading">
               OUR GOALS
             </p>
 
-            {/* goals description*/}
-            <div className="goals-desc">
+          {/* goals description*/}
 
-              <p>
-                Inland Empire Software Development (IESD) is an independent
-                community organization dedicated to bringing community
-                members together to network, educate and share knowledge
-                - all with the goal of increasing the technology presence
-                in the Inland Empire. <br/><br/>
-                IESD is spearheaded by an organizing group that is
-                passionate about increasing technology awareness by
-                increasing industry professional involvement
-                with the community, exposing developers to new technologies
-                and creating partnerships with community entities that
-                benefit the community.
-              </p>
-
-            </div>
-
-          </div>
+            <p className="goals-desc">
+              Inland Empire Software Development (IESD) is an independent
+              community organization dedicated to bringing community
+              members together to network, educate and share knowledge
+              - all with the goal of increasing the technology presence
+              in the Inland Empire.
+              IESD is spearheaded by an organizing group that is
+              passionate about increasing technology awareness by
+              increasing industry professional involvement
+              with the community, exposing developers to new technologies
+              and creating partnerships with community entities that
+              benefit the community.
+            </p>
 
         </div>
 
