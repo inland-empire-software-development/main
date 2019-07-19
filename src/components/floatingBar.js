7673785caaea7aa1532b17851206a06ab4a339d3
yzhan407 <hehehe2333@hotmail.com>
2019-07-18 22:37:14 -0700

    modified according to requirements

--- src/components/floatingBar.js
+++ src/components/floatingBar.js
@@ -1,15 +1,17 @@
 function FloatingBar() {
   return (
-    <floatingBar className="floating-bar-main">
+    <floatingbar className="floating-bar-main">
       <div className="left-floating-bar">
         <a target="_blank" href="tel:18004370267">
-          <i class="fas fa-phone" data-fa-transform="right-10 up-40"></i>
+          <div id="phone-svg">
+            <i class="fas fa-phone fa-2x fa-border" />
+          </div>
         </a>
         <a target="_blank" href="mailto:community@ie-sd.com">
-          <i class="fas fa-envelope" data-fa-transform="right-10 up-30"></i>
+          <i class="fas fa-envelope fa-border"></i>
         </a>
         <a>
-          <i class="far fa-comment-dots" data-fa-transform="right-10 up-20"></i>
+          <i class="far fa-comment-dots fa-border" />
         </a>
         <div className="floating-bar-contact">contact:</div>
       </div>
@@ -17,22 +19,22 @@ function FloatingBar() {
       </div>
       <div className="right-floating-bar">
         <a target="_blank" href="https://twitter.com/iesdevinc">
-          <i class="fab fa-twitter" data-fa-transform="right-16 up-119"></i>
+          <i class="fab fa-twitter fa-lg fa-border"></i>
         </a>
         <a target="_blank" href="https://www.facebook.com/groups/IESoftwareDevelopment/">
-          <i class="fab fa-facebook-f" data-fa-transform="right-16 up-103"></i>
+          <i class="fab fa-facebook-f fa-lg fa-border" />
         </a>
         <div className="share-inner-div">share:</div>
         <hr className="floating-bar-inner-line"/>
         <a target="_blank" href="https://www.meetup.com/iesd-meetup/">
-          <i class="fab fa-meetup" data-fa-transform="right-14 up-103"></i>
+          <i class="fab fa-meetup fa-border"></i>
         </a>
         <a target="_blank" href="https://bit.ly/2C0umfQ">
-          <i class="fab fa-slack" data-fa-transform="right-14 up-90"></i>
+          <i class="fab fa-slack fa-border"></i>
         </a>
         <div className="join-inner-div">join:</div>
       </div>
-    </floatingBar>
+    </floatingbar>
   );
 }
 
