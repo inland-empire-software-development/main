ca852f9aad0b8c7931bc3c6a918ee0cc3491da4b
lloan <lloanalas@outlook.com>
2019-07-20 03:00:24 -0700

    css tweaked to matched overall layout alignment

--- src/components/sponsors.js
+++ src/components/sponsors.js
@@ -1,20 +1,25 @@
 function Sponsors() {
-	// initial render
-	return (
-		<div id="sponsors" className='grid-container'>
-			<h1 className="avenir-bold">OUR SPONSORS</h1>
-			<p>The work of Inland Empire Software Development, IESD, is made possible by the following sponsors</p>
-			<div className='"sponsor-gallery"'>
-				<div className="column-4">
-					<img width="200px" src='../../static/images/desktop/UCRlogo.png'/>
-				</div>
-				<div className="column-4">
-					<img width="200px" src='../../static/images/desktop/ExCITElogo.jpg'/>
-				</div>
-			</div>
-		</div>
-		)
-	
-	}
-	
-	export default Sponsors;
+  const sponsorList = [
+    '../../static/images/desktop/UCRlogo.png',
+    '../../static/images/desktop/ExCITElogo.jpg',
+  ];
+  // initial render
+  return (
+    <div id="sponsors" className='grid-container'>
+      <div className="column-20 pre-2 leader-3 trailer-3">
+        <h1>Our Sponsors</h1>
+        <p>The work at <strong>IESD</strong> is made
+         possible by the following sponsors</p>
+        <div className="sponsor-gallery">
+          {sponsorList.map((sponsor) => (
+            <div className="column-6">
+              <img src={sponsor}/>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Sponsors;
