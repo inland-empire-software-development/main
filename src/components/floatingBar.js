9953752352bd1c6b38e8eb7ac789610ad43ac999
lloan <lloanalas@outlook.com>
2019-07-20 13:33:46 -0700

    wip - new icons, need alignment - use event listeners instead of anchor tags

--- src/components/floatingBar.js
+++ src/components/floatingBar.js
@@ -1,6 +1,6 @@
 function FloatingBar() {
   return (
-    <floatingbar className="floating-bar-main">
+    <div className="floating-bar-main">
       <div className="left-floating-bar">
         <a target="_blank" href="tel:18004370267">
           <div id="phone-svg">
@@ -10,31 +10,29 @@ function FloatingBar() {
         <a target="_blank" href="mailto:community@ie-sd.com">
           <i class="fas fa-envelope fa-border"></i>
         </a>
-        <a>
-          <i class="far fa-comment-dots fa-border" />
-        </a>
         <div className="floating-bar-contact">contact:</div>
       </div>
       <div className="floating-empty-div">
       </div>
+
       <div className="right-floating-bar">
-        <a target="_blank" href="https://twitter.com/iesdevinc">
-          <i class="fab fa-twitter fa-lg fa-border"></i>
+        <a target="_blank" className="social-twitter-link" href="https://twitter.com/iesdevinc">
+          <img src="../../static/logos/twitter.svg" />
         </a>
-        <a target="_blank" href="https://www.facebook.com/groups/IESoftwareDevelopment/">
-          <i class="fab fa-facebook-f fa-lg fa-border" />
+        <a target="_blank" className="social-facebook-link" href="https://www.facebook.com/groups/IESoftwareDevelopment/">
+          <img src="../../static/logos/facebook.svg" />
         </a>
         <div className="share-inner-div">share:</div>
         <hr className="floating-bar-inner-line"/>
-        <a target="_blank" href="https://www.meetup.com/iesd-meetup/">
-          <i class="fab fa-meetup fa-border"></i>
+        <a target="_blank" className="social-mmetup-link" href="https://www.meetup.com/iesd-meetup/">
+          <img src="../../static/logos/meetup.svg" />
         </a>
-        <a target="_blank" href="https://bit.ly/2C0umfQ">
-          <i class="fab fa-slack fa-border"></i>
+        <a target="_blank" className="social-slack-link" href="https://bit.ly/2C0umfQ">
+          <img src="../../static/logos/slack.svg" />
         </a>
         <div className="join-inner-div">join:</div>
       </div>
-    </floatingbar>
+    </div>
   );
 }
 
