65d8e55266024e30057be7cdefb28cc60d3b5fd5
holyoke <jasonebueng@gmail.com>
2019-07-24 22:00:49 -0700

    tweaks header

--- src/components/events/Events.js
+++ src/components/events/Events.js
@@ -21,7 +21,7 @@ function Events() {
 
   return (
     <div className={calciteClass}>
-      <h1 className="event-header leader-1">Upcoming Events</h1>
+      <h1 className="event-header pre-1 leader-1">Upcoming Events</h1>
       <div className="events-list pre-1">
         {eventItems(events)}
       </div>
