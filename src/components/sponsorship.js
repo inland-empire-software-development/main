7836a0570ca385c127fac770f290c0ae9e8a6a78
chris pierre <chrispierrelouis.cpl@gmail.com>
2019-08-03 15:34:47 -0800

    fixed media issues

--- src/components/sponsorship.js
+++ src/components/sponsorship.js
@@ -1,36 +1,35 @@
 function Sponsorship() {
-
-    function handleClick(e){
-        e.preventDefault();
-        console.log('One-time')
-    }
-
-    function hanldeRecurring(e){
-        e.preventDefault();
-        console.log('Recurring Sponsorship');
-    }
+  function handleClick(e) {
+    e.preventDefault();
+    console.log("One-time");
+  }
+  function hanldeRecurring(e) {
+    e.preventDefault();
+    console.log("Recurring Sponsorship");
+  }
   return (
-    <div className="grid-container">
+    <div id="sponsorship" className="grid-container">
       {/* left side */}
-      <div id="sponsor-1" className="column-12 pre-2 leader-1 trailer-1">
+      <div id="sponsorship-description" className="column-14">
         {/* sponsor-title */}
-        <p id ='sponsor-title'>Interested in becoming a Sponsor?</p>
+        <p id="sponsor-title">Interested in becoming a Sponsor?</p>
         <p>
-          To get started, (link goes here) to see the sponsorship packages we have
-          available. Once you're ready, you can securely process payments using
-          the buttons on your right.
+          To get started, (link goes here) to see the sponsorship packages we
+          have available. Once you"re ready, you can securely process payments
+          using the buttons on your right.
         </p>
       </div>
-{/* Buttons below */}
-      <section>
-        <div id ='buttons'className='column-4 leader-2 trailer-1'>
-          <button  onClick ={handleClick} id = 'one-time' className='btn'  >One-time Sponsor</button>
-          <button  onClick ={hanldeRecurring} id= 'recurring'className='btn'>Recurring Sponsorship</button>
-      </div> 
-      </section>
+      {/* Buttons below */}
+      <div className="column-10 phone-column-6 tablet-column-12">
+        <button onClick={handleClick} id="one-time">
+          One-time Sponsor
+        </button>
+        <button onClick={hanldeRecurring} id="recurring" className="trailer-1">
+          Recurring Sponsorship
+        </button>
+      </div>
     </div>
   );
 }
 
 export default Sponsorship;
-
