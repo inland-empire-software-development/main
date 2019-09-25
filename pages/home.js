ddd9b88a29807a4d42789de0c26ee6058fe63d87
lloan <lloanalas@outlook.com>
2019-09-25 15:46:57 -0700

    added heroEvent component - wip

--- pages/home.js
+++ pages/home.js
@@ -6,32 +6,14 @@ import '../node_modules/@fortawesome/fontawesome-free/js/all';
 import '../sass/index.scss';
 
 import {withRouter} from 'next/router';
-import Nav from "../src/components/Nav";
+import Hero from "../src/components/Hero";
 
 // import Fetch from 'isomorphic-unfetch';
 function App() {
   // initial render
   return (
     <div className="home">
-      <section id="hero" >
-
-        <video autoPlay muted loop id="iesd-video">
-          <source
-            src="../../static/video/main/hero-main.mp4"
-            type="video/mp4"
-          />
-        </video>
-        <div className="uk-overlay-primary uk-position-cover"/>
-        <div className="container-full">
-          <div className="uk-container">
-
-            {/* nav start*/}
-            <Nav />
-            {/* nav end*/}
-
-          </div>
-        </div>
-      </section>
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,32 @@
+import Nav from "./Nav";
+import HeroEvent from "./HeroEvent";
+
+function Hero() {
+  return (
+    <section id="hero">
+
+      {/* TODO: Find a way to NOT render this under 960px */}
+      <video className="uk-visible@m" autoPlay muted loop id="iesd-video">
+        <source
+          src="../../static/video/main/hero-main.mp4"
+          type="video/mp4"
+        />
+      </video>
+
+      <div className="uk-overlay-primary uk-position-cover"/>
+
+      <div className="container-full">
+        <div className="uk-container">
+
+          {/* nav start*/}
+          <Nav />
+          {/* nav end*/}
+
+          <HeroEvent />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
