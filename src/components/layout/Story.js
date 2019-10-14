92b84b36eb47af628f5afb4ba83e37a23c5555a3
lloan <lloanalas@outlook.com>
2019-10-14 07:44:07 -0700

    story wip - contents added, api call added

--- src/components/layout/Story.js
+++ src/components/layout/Story.js
@@ -1,14 +1,57 @@
+import {Query} from "react-apollo";
+import gql from "graphql-tag";
+import MemberList from '../MemberList';
+
+export const storiesQuery = gql`
+ query Stories {
+  stories {
+    nodes {
+      details {
+        content
+        facebook
+        image {
+          sourceUrl
+        }
+        github
+        instagram
+        linkedin
+        name
+        title
+        twitter
+      }
+      order {
+        position
+      }
+    }
+  }
+}
+`;
+
 export default function Story() {
   return (
-    <div id="story-container" className="container-full black-white" >
-      <div id="story" className="uk-container">
-        <div uk-slider="true">
-          <div className="success-image-container">
-            <img alt={name} src={imageURL} />
-            <div className="success-image-overlay"></div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Query query={storiesQuery} >
+      {({loading, error, data}) => {
+        if (error) return <aside>Error loading stories!</aside>;
+        if (loading) return <div>Loading</div>;
+
+        const stories = data.stories.nodes.length;
+        const story = data.stories.nodes[Math.floor(Math.random() * stories )];
+
+        console.log(story);
+        return (
+          <div id="story-container"
+            className="container-full black-white" >
+            <div id="story" className="uk-container">
+              <img className="story-image"
+                   alt="Felipe"
+                   src={story.details.image.sourceUrl}
+              />
+              <p className="story-content">
+                {story.details.content}
+              </p>
+            </div>
+          </div>);
+      }}
+    </Query>
   );
 }
