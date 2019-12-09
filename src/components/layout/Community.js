e827e3b75f21c4a36dd98fbacbbf3660140a13c4
VelazquezLuis <javiervelazquez113@yahoo.com>
2019-12-08 22:18:50 -0800

    took off hardcoded code

--- src/components/layout/Community.js
+++ src/components/layout/Community.js
@@ -1,8 +1,7 @@
 import {Query} from "react-apollo";
 import gql from "graphql-tag";
-import Loader from '../global/Loader';
-
 import MemberList from "../MemberList";
+import Loader from '../global/Loader';
 
 export const communityQuery = gql `
   query Community {
@@ -27,39 +26,28 @@ export const communityQuery = gql `
   
 `;
 
-
-
 export default function community() {
   return (
     <Query query={communityQuery} >
+      {({loading, error, data}) => {
+        if (error) return <aside>Error loading community!</aside>;
+        if (loading) return <Loader />;
 
-    {({loading, error, data}) => {
-      if (error) return <aside>Error loading community!</aside>;
-      if (loading) return <Loader />;
-
-  return (
-    <div id="community-container" className="container-full" style={{
-      backgroundImage:
-        "url(\"/static/images/desktop/iesd-bg-light.jpg\")",
-    }}>
-      <div id="community" className="uk-container">
-        <div uk-slider="true">
-          <ul className="uk-slider-items uk-child-width-1-2@s
-      uk-child-width-1-4@m uk-grid">
-             <MemberList
-            label="Community"
-            warning={true}
-            members={data.community.nodes.sort((a, b) =>
+        return (
+          <div id="community-container" className="container-full" style={{
+            backgroundImage:
+              "url(\"/static/images/desktop/iesd-bg-light.jpg\")",
+          }}>
+            <MemberList
+              label="Community"
+              warning={true}
+              members={data.community.nodes.sort((a, b) =>
                 a.order.position - b.order.position).map((member) => {
-                return {            
-                  image: member.details.imageCard.sourceUrl
-              };
-            })}/>
-          </ul>
-          <ul className="uk-slider-nav uk-dotnav uk-flex-center uk-margin"></ul>
-        </div>
-      </div>
-    </div>
+                return {
+                  image: member.details.imageCard.sourceUrl,
+                };
+              })}/>
+          </div>
         );
       }}
     </Query>
