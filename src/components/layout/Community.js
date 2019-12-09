b3b6c6cca0c892e9fcfd9f0dfb4f24a57dc92c77
lloan <lloanalas@outlook.com>
2019-12-08 22:39:59 -0800

    slider updated - wip - not using MemberList

--- src/components/layout/Community.js
+++ src/components/layout/Community.js
@@ -33,20 +33,50 @@ export default function community() {
         if (error) return <aside>Error loading community!</aside>;
         if (loading) return <Loader />;
 
+        const community = data.community.nodes.sort((a, b) =>
+          a.order.position - b.order.position);
+
+        console.log(community);
         return (
-          <div id="community-container" className="container-full" style={{
-            backgroundImage:
+          <div
+            id="community-container"
+            className="container-full"
+            style={{backgroundImage:
               "url(\"/static/images/desktop/iesd-bg-light.jpg\")",
-          }}>
-            <MemberList
-              label="Community"
-              warning={true}
-              members={data.community.nodes.sort((a, b) =>
-                a.order.position - b.order.position).map((member) => {
-                return {
-                  image: member.details.imageCard.sourceUrl,
-                };
-              })}/>
+            }}>
+
+            <div className="container-full memberList-container">
+              <div className="uk-container">
+                <p className="memberlist-header heading">{ }</p>
+                <div uk-slider="true">
+                  <ul className="uk-slider-items uk-child-width-1-2 uk-child-width-1-3@s
+            uk-child-width-1-5@m uk-grid">
+                    {community.map((moment, index) => {
+                      const {
+                        dateOfEvent,
+                        description,
+                        imageCard,
+                        imageModal} = moment.details;
+
+                      return (<li key={index}>
+                        <div className="uk-card uk-card-default">
+                          <div className="uk-card-media-top">
+                            <img uk-image="true" src={
+                                  imageCard ?
+                                    imageCard.sourceUrl :
+                                    "/static/images/desktop/placeholder.jpg"
+                            } alt={dateOfEvent} title={description} />
+                          </div>
+                        </div>
+                      </li>
+                      );
+                    },
+                    )}
+                  </ul>
+                  <ul className="uk-slider-nav uk-dotnav uk-flex-center uk-margin"/>
+                </div>
+              </div>
+            </div>
           </div>
         );
       }}
