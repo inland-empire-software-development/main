a73b618db681dffda978d0f3e2ef5fe277f85b12
lloan <lloanalas@outlook.com>
2019-12-14 12:50:16 -0800

    mentorship page wip - initial skeleton - porting from calcite

---
+++ src/components/PageTitle.js
@@ -0,0 +1,17 @@
+function PageTitle() {
+  return (
+    <header id="pageTitle"
+      className="grid-container"
+      style={{backgroundImage: `url(../../static/images/mentorship/page-header.jpg)`}}>
+      <div className="overlay-background-header">
+        <div className="uk-container">
+          <h1>
+            Mentorship
+          </h1>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default PageTitle;
