aebba912ef801315b3321ef7d3a3d80e44cd09b0
lloan <lloanalas@outlook.com>
2019-12-17 13:09:07 -0800

    update alignment for list in page intro for volunteers

--- pages/volunteer.js
+++ pages/volunteer.js
@@ -25,10 +25,10 @@ function Volunteer() {
             <p className="body-content">
               The following are currently open positions for the nonprofit organization, <strong>Inland Empire Software Development, Inc</strong> (IESD).
               Although these are volunteer positions for a nonprofit organization, we do provide letters of recommendation and references.
-              If you know anyone outside of IESD who may be interested, share this list with them. We can use all the help we can get.
+              If you know anyone outside of IESD who may be interested, share this list with them. Our community can always use an extra hand.
             </p>
-            <p className="body-content">Benefits include:</p>
-            <ul>
+            <p className="body-content uk-margin-remove">Benefits include:</p>
+            <ul className="uk-margin-remove">
               <li>Get experience working on a large team</li>
               <li>Making a difference in your community</li>
               <li>Interacting with industry professionals</li>
