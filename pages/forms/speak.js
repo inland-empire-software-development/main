9fcb2a3413fe49d20044f225a3e2f43cc869389e
lloan <lloanalas@outlook.com>
2019-12-20 13:45:29 -0800

    add speak page

---
+++ pages/forms/speak.js
@@ -0,0 +1,12 @@
+function Speak() {
+  return (
+    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScLlV350hb9z7g3AAZPsqSgfozhdDRw50sej_gEWwA0KTYtzQ/viewform?embedded=true" style={{
+      width: '100%',
+      height: '1800px',
+      margin: '30px auto 0 auto',
+      display: 'block',
+    }}>Loading…</iframe>
+  );
+}
+
+export default Speak;
